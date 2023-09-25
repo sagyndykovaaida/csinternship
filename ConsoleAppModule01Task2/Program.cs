a6e3014a1ed290a245fc304637ef197594ba1794
sagyndykovaaida <lvnidass@gmail.com>
2023-09-25 23:57:19 +0600

    the second task in module 1

--- ConsoleAppModule01Task2/Program.cs
+++ ConsoleAppModule01Task2/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleAppModule01Task2
     {
         static void Main(string[] args)
         {
+            Console.Write("Count: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                int[] nums = new int[n]; 
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int num))
+                    {
+                        nums[i] = num;
+                    }
+                }
+
+                int sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    sum += nums[i];
+                }
+                Console.WriteLine($"Summa: {sum}");
+                Console.ReadKey();
+            }
         }
     }
 }
