b0c499861c8ab4d967243a5a533919472e9657dd
sagyndykovaaida <lvnidass@gmail.com>
2023-10-06 22:43:18 +0600

    add tasks from module04

---
+++ ConsoleAppModule04/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppModule04
+{
+    internal class practice1
+    {
+        static void Main()
+        {
+            Student[] students = new Student[5]
+            {
+                new Student("Student1","M","B", 2,new int[] {5,5,5,5,5}),
+                new Student("Student2","E","K", 3,new int[] {5,4,5,4,5}),
+                new Student("Student3","I","A", 1,new int[] {5,5,4,5,5}),
+                new Student("Student4","D","A", 1,new int[] {4,5,3,2,2}),
+                new Student("Student5","N","S", 3,new int[] {3,4,3,5,2})
+            };
+            var sort = students.OrderBy(student => student.AverageGrade()).ToArray();
+
+            Console.WriteLine("Students with grades 4 or 5: ");
+            foreach (var student in sort)
+            {
+                if (student.Grades.All(grade => grade >= 4))
+                {
+                    Console.WriteLine($"FIO: {student.LastName}.{student.FirstName}.{student.MiddleName}, Group: {student.GroupNumber}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    struct Student
+    {
+        public string LastName, FirstName, MiddleName;
+        public int GroupNumber;
+        public int[] Grades;
+
+        public Student(string lastName, string firstName, string middleName, int groupNumber, int[] grades)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+            GroupNumber = groupNumber;
+            Grades = grades;
+        }
+
+        public double AverageGrade()
+        {
+            return Grades.Average();
+        }
+    }
+}
---
+++ ConsoleAppModule04/practice2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppModule04
+{
+    internal class practice2
+    {
+        static void Main()
+        {
+            List<Animal> animals = new List<Animal>
+            {
+                new Carnivore(1, "Лев", 5.5),
+                new Omnivore(2, "Медведь", 3.0),
+                new Herbivore(3, "Зебра", 10.0),
+            };
+
+            animals.Sort();
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            var first5Names = animals.Take(5).Select(a => a.Name);
+            Console.WriteLine("Первые 5 имен: " + string.Join(", ", first5Names));
+
+            Console.WriteLine("Последние 3 идентификатора:");
+            for (int i = animals.Count - 3; i < animals.Count; i++)
+            {
+                Console.WriteLine(animals[i].Id);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal : IComparable<Animal>
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Type { get; }
+        public abstract double FoodConsumption();
+
+        public Animal(int id, string name, string type)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+        }
+
+        public int CompareTo(Animal other)
+        {
+            int foodComparison = FoodConsumption().CompareTo(other.FoodConsumption());
+            if (foodComparison == 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+            return -foodComparison;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name} ({Type}) - {FoodConsumption()} кг в день";
+        }
+    }
+
+    class Carnivore : Animal
+    {
+        public double MeatConsumption { get; }
+
+        public Carnivore(int id, string name, double meatConsumption)
+            : base(id, name, "Хищник")
+        {
+            MeatConsumption = meatConsumption;
+        }
+
+        public override double FoodConsumption()
+        {
+            return MeatConsumption;
+        }
+    }
+
+    class Omnivore : Animal
+    {
+        public double PlantConsumption { get; }
+
+        public Omnivore(int id, string name, double plantConsumption)
+            : base(id, name, "Всеядное")
+        {
+            PlantConsumption = plantConsumption;
+        }
+
+        public override double FoodConsumption()
+        {
+            return PlantConsumption;
+        }
+    }
+
+    class Herbivore : Animal
+    {
+        public double GrassConsumption { get; }
+
+        public Herbivore(int id, string name, double grassConsumption)
+            : base(id, name, "Травоядное")
+        {
+            GrassConsumption = grassConsumption;
+        }
+
+        public override double FoodConsumption()
+        {
+            return GrassConsumption;
+        }
+    }
+}
