bac6e5230c798ad43c17d21b5e1b1106758febea
sagyndykovaaida <lvnidass@gmail.com>
2023-10-12 15:30:25 +0600

    tasks module06

---
+++ Module06/Bankomat/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    public class Client
+    {
+        public string Name { get; }
+        public int ClientID { get; }
+        public Account BankAccount { get; }
+        public string Password { get; }
+
+        public Client(string name, int clientID, string password)
+        {
+            Name = name;
+            ClientID = clientID;
+            Password = password;
+            BankAccount = new Account(clientID);
+        }
+
+        public bool CheckPassword(string password)
+        {
+            return Password == password;
+        }
+    }
+}
