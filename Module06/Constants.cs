c95e852ead43940a124977d3fd19e4bcf2220027
sagyndykovaaida <lvnidass@gmail.com>
2023-10-12 15:26:38 +0600

    tasks module06

---
+++ Module06/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module06
+{
+    public class Constants
+    {
+        public const string name = "Aida";
+        public const int id = 888;
+
+      
+    }
+
+  
+}
