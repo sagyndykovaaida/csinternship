208c62946dc7c70bb4735111116e5f720bd43b60
sagyndykovaaida <alievnasg@gmail.com>
2023-10-27 01:42:37 +0600

    practice tasks Module08

---
+++ Module08/ConsoleAppModule08/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleAppModule08
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("начальное значение диапазона: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.Write("конечное значение диапазона: ");
+            int end = int.Parse(Console.ReadLine());
+
+            RangeOfArray range = new RangeOfArray();
+            int[] arr = range[start, end];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class RangeOfArray
+    {
+        private int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+
+        public int[] this[int start, int end]
+        {
+            get
+            {
+                if (start > end)
+                {
+                    Console.WriteLine("Ошибка");
+                }
+
+                int n = end - start + 1;
+                int[] result = new int[n];
+
+                for (int i = 0, j = start; i < n; i++, j++)
+                {
+                    result[i] = arr[j];
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ Module08/ConsoleAppModule08/practice3.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppModule08
+{
+    internal class practice3
+    {
+        static void Main()
+        {
+            Sales salesData = new Sales();
+
+            salesData[1] = 100;
+            salesData[2] = 150;
+            salesData[3] = 200;
+            salesData[4] = 250;
+            salesData[5] = 300;
+
+            double[] forecast = salesData.SalesForecast();
+
+            Console.WriteLine("продаж на следующие 3 месяца:");
+            for (int i = 0; i < forecast.Length; i++)
+            {
+                Console.WriteLine($"Месяц {i + 6}: {forecast[i]}");
+            }
+
+            Console.ReadKey();
+        }
+        class Sales
+        {
+            private double[] sales = new double[5]; 
+
+            public double this[int month]
+            {
+                get
+                {
+                    if (month >= 1 && month <= 5)
+                    {
+                        return sales[month - 1];
+                    }
+                    return 0; 
+                }
+                set
+                {
+                    if (month >= 1 && month <= 5)
+                    {
+                        sales[month - 1] = value;
+                    }
+                }
+            }
+
+            public double calcAverage()
+            {
+                double sum = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    sum += sales[i];
+                }
+
+                return sum / 5;
+            }
+
+            public double[] SalesForecast()
+            {
+                double averageSales = calcAverage();
+                double[] forecast = new double[3];
+
+                for (int i = 0; i < 3; i++)
+                {
+                    forecast[i] = averageSales;
+                }
+
+                return forecast;
+            }
+        }
+    }
+}
