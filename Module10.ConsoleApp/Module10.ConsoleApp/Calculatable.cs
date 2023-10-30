d154d45ae41cac1d55e808ee8fb1256a1add28c7
sagyndykovaaida <alievnasg@gmail.com>
2023-10-30 18:33:59 +0600

    Module 10: Interface

---
+++ Module10.ConsoleApp/Module10.ConsoleApp/Calculatable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.ConsoleApp
+{
+    public interface ICalculatable
+    {
+        double Add(double a, double b);
+        double Multiply(double a, double b);
+        double Subtract(double a, double b);
+        double Divide(double a, double b);
+    }
+
+    public class Calculator : ICalculatable
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+    }
+
+    static void Main()
+    {
+        ICalculatable calculatable = new Calculator();
+        double result1 = calculatable.Add(2, 5);
+        double result2 = calculatable.Subtract(20, 5);
+        double result3 = calculatable.Multiply(2, 5);
+        double result4 = calculatable.Divide(10, 2);
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(result3);
+        Console.WriteLine(result4);
+        Console.ReadKey();
+    }
+}
---
+++ Module10.ConsoleApp/Module10.ConsoleApp/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.ConsoleApp
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName , string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+        
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine($"Full name: {FirstName} {LastName}");
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Person other)
+            {
+                return FirstName == other.FirstName && LastName == other.LastName;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FirstName, LastName);
+        }
+    }
+    public class Student : Person
+    {
+        public List<Teacher> Teachers { get;} = new List<Teacher>();
+
+        public Student(string firstName, string lastName) : base(firstName , lastName)
+        {
+            
+        }
+
+        public void addTeacher(Teacher teacher)
+        {
+            Teachers.Add(teacher);
+        }
+        public override void Print() {
+            Console.WriteLine($"Student name:{FirstName} {LastName}");
+            Console.WriteLine("Teachers : ");
+            for(int i = 0; i< Teachers.Count;i++) {
+                Console.WriteLine(Teachers[i]);
+
+            }
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public List <Student> students = new List<Student>();
+
+        public Teacher(string firstName, string lastName) : base(firstName, lastName)
+        {
+        }
+
+        public void addStudent(Student student)
+        {
+            students.Add(student);
+            student.addTeacher(this);
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Teacher: {FirstName} {LastName}");
+            Console.WriteLine("Students:");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"  {student}");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Aida", "Alievna");
+
+            Student student1 = new Student("Aida", "Alievna");
+
+            Teacher teacher1 = new Teacher("Yevgeniy", "Gertsen");
+
+            student1.addTeacher(teacher1);
+
+            teacher1.addStudent(student1);
+
+            List<Person> people = new List<Person> { person1, student1, teacher1, };
+
+            foreach (var person in people)
+            {
+                person.Print();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
