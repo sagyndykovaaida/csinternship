bf68bc8d0d00c5b4c2096d8ff02259ab2e0cfc7e
sagyndykovaaida <alievnasg@gmail.com>
2023-11-17 13:48:12 +0600

    add module11's practice tasks

---
+++ Module11.ConsoleApp/Module11.ConsoleApp/Delivery.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace Module11.ConsoleApp
+{
+    public enum Status
+    {
+        Created,
+        Shipped,
+        Delivered
+    }
+
+    public class Info
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public Status Status { get; set; }
+        public string Description { get; set; } 
+
+    }
+    public class DbHandler
+    {
+        private LiteDatabase _db;
+
+        public DbHandler(string databasePath)
+        {
+            _db = new LiteDatabase(databasePath);
+        }
+
+        public LiteCollection<Info> GetCollection()
+        {
+            return (LiteCollection<Info>)_db.GetCollection<Info>("waybills");
+        }
+    }
+
+    public class FileHandler
+    {
+        public void SaveToFile(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+
+        public void CopyFile(string sourcePath, string destinationPath)
+        {
+            File.Copy(sourcePath, destinationPath, true);
+        }
+    }
+
+    public class CourierService
+    {
+        private readonly DbHandler _dbHandler;
+        private readonly FileHandler _fileHandler;
+
+        public CourierService(string databasePath)
+        {
+            _dbHandler = new DbHandler(databasePath);
+            _fileHandler = new FileHandler();
+        }
+
+        public void CreateOrder(string senderName, string receiverName)
+        {
+            
+            var collection = _dbHandler.GetCollection();
+
+            var i = new Info
+            {
+                Id = GetNextOrderId(),
+                CreateDate = DateTime.Now,
+                SenderName = senderName,
+                ReceiverName = receiverName,
+                Status = Status.Created
+            };
+
+            collection.Insert(i);
+
+            i.Description = $"ID: {i.Id}\n" +
+                                  $"Creation Date: {i.CreateDate}\n" +
+                                  $"Sender: {i.SenderName}\n" +
+                                  $"Receiver: {i.ReceiverName}\n" +
+                                  $"Status: {i.Status}";
+
+            _fileHandler.SaveToFile($"order{i.Id}.txt", i.Description);
+        }
+
+        public List<Info> GetAllOrders()
+        {
+            var collection = _dbHandler.GetCollection();
+            return collection.FindAll().Select(w => new Info
+            {
+                Id = w.Id,
+                CreateDate = w.CreateDate,
+                SenderName = w.SenderName,
+                ReceiverName = w.ReceiverName
+            }).ToList();
+        }
+
+        public Info GetOrderById(int waybillId)
+        {
+            var collection = _dbHandler.GetCollection();
+            return collection.FindOne(w => w.Id == waybillId);
+        }
+
+        private int GetNextOrderId()
+        {
+            var collection = _dbHandler.GetCollection();
+            return collection.Count() > 0 ? collection.Max(w => w.Id) + 1 : 1;
+        }
+    }
+    internal class Delivery
+    {
+        static void Main()
+        {
+
+            Console.Write("имя отправителя: ");
+            string senderName = Console.ReadLine();
+
+            Console.Write("имя получателя: ");
+            string receiverName = Console.ReadLine();
+
+            var courierService = new CourierService("C:\\temp\\courier.db");
+
+            try
+            {
+                courierService.CreateOrder(senderName, receiverName);
+                Console.WriteLine("КН успешно создана.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при создании КН: {ex.Message}");
+            }
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Module11.ConsoleApp/Module11.ConsoleApp/EmployeeStruct.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module11.ConsoleApp
+{
+    public enum Vacancies
+    {
+        Developer,
+        Manager,
+        Clerk
+    }
+
+    public struct Employee
+    {
+        public string Name;
+        public Vacancies Vacancy;
+        public int[] HireDate;
+        public int Salary;
+        public Employee(string name, Vacancies vacancy, int[] hireDate, int salary)
+        {
+            Name = name;
+            Vacancy = vacancy;
+            HireDate = hireDate;
+            Salary = salary;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Вакансия: {Vacancy}");
+            Console.WriteLine($"Дата приема на работу: {HireDate[0]}.{HireDate[1]}.{HireDate[2]}");
+            Console.WriteLine($"Зарплата: {Salary}");
+        }
+    }
+    internal class EmployeeStruct
+    {
+        static void Main()
+        {
+            Employee employee1 = new Employee("User User", Vacancies.Manager, new int[] { 2022, 11, 16 }, 555555);
+            Employee employee2 = new Employee("Test Test", Vacancies.Clerk, new int[] { 2021, 8, 25 }, 355555);
+
+            Console.WriteLine("Инфо о сотруднике 1:");
+            employee1.DisplayInfo();
+
+            Console.WriteLine("Инфо о сотруднике 2:");
+            employee2.DisplayInfo();
+            Console.ReadKey();
+        }
+
+    }
+}
