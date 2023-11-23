4ee86e3ecf864d70bc00b0799fcd879ac32bc25d
sagyndykovaaida <alievnasg@gmail.com>
2023-11-23 14:27:57 +0600

    add hw task module12: game car

---
+++ CartConversion/CartConversion/Car.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartConversion
+{
+    public abstract class Car
+    {
+        public event EventHandler<string> RaceEvent;
+        public event EventHandler<string> FinishEvent;
+        public int CurrentSpeed { get; private set; } = 0;
+        public int MaxSpeed { get; set; }
+        public bool IsDead { get; private set; } = false;
+
+        public abstract string Model { get; }
+
+        public virtual void Accelerate(int delta)
+        {
+            if (IsDead)
+            {
+                RaceEvent?.Invoke(this, $"{Model} is dead");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    IsDead = true;
+                    FinishEvent?.Invoke(this, $"{Model} has finished the race!");
+                }
+                else
+                {
+                    RaceEvent?.Invoke(this, $"{Model} is racing at {CurrentSpeed} speed.");
+                }
+            }
+        }
+    }
+
+    public class RaceCar : Car
+    {
+        public override string Model => "RaceCar";
+
+        public RaceCar(int maxSpeed)
+        {
+            MaxSpeed = maxSpeed;
+        }
+    }
+
+    public class SportsCar : Car
+    {
+        public override string Model => "SportsCar";
+
+        public SportsCar(int maxSpeed)
+        {
+            MaxSpeed = maxSpeed;
+        }
+    }
+
+    public class RacingGame
+    {
+        public event EventHandler<string> RaceStartEvent; 
+
+        public void StartRace()
+        {
+            RaceStartEvent?.Invoke(this, "Race is starting!");
+        }
+    }
+
+}
---
+++ CartConversion/ConsoleAppModule12/Program.cs
@@ -0,0 +1,34 @@
+﻿using CartConversion;
+using System;
+
+namespace ConsoleAppModule12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RacingGame game = new RacingGame();
+
+            RaceCar raceCar = new RaceCar(200);
+            SportsCar sportsCar = new SportsCar(180);
+
+            raceCar.RaceEvent += DisplayMessage;
+            raceCar.FinishEvent += DisplayMessage;
+
+            sportsCar.RaceEvent += DisplayMessage;
+            sportsCar.FinishEvent += DisplayMessage;
+
+            game.StartRace();
+
+            raceCar.Accelerate(20);
+            sportsCar.Accelerate(15);
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayMessage(object sender, string e)
+        {
+            Console.WriteLine($"[Game]: {e}");
+        }
+    }
+}
